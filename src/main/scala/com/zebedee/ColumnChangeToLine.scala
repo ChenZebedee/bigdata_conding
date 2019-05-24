bc0a617d7cec6777ad93ddee103c5ee38370cbcc
ChenZebedee <shaodi.chen@manaowan.com>
2019-05-24 17:33:07 +0800

    1

--- src/main/scala/com/zebedee/ColumnChangeToLine.scala
+++ src/main/scala/com/zebedee/ColumnChangeToLine.scala
@@ -3,7 +3,6 @@ package com.zebedee
 import java.util
 import java.util.Properties
 
-import com.alibaba.fastjson.JSON
 import com.mnw.data.config.HBaseData
 import com.mnw.data.constant.PunctuationConst
 import com.mnw.utils.{DataUtils, FileUtils, HbaseUtils}
@@ -18,86 +17,86 @@ import org.apache.spark.{SparkConf, SparkContext}
   *
   */
 object ColumnChangeToLine {
-  def main(args: Array[String]): Unit = {
-    val configFileData1 = getFileData(args(0))
-    val conf = new SparkConf().setAppName("paSpark")
-    val sc = new SparkContext(conf)
-    val fileData1 = getRdd4File(configFileData1._1, sc, configFileData1._2, configFileData1._3, configFileData1._4)
-    val configFileData2 = getFileData(args(1))
-    val fileData2 = getRdd4File(configFileData2._1, sc, configFileData2._2, configFileData2._3, configFileData2._4)
-    fileData1.join(fileData2).map(value => {
-      val outKey = value._1
-      val outValue = value._2._1 ++ value._2._2
-      (outKey, outValue)
-    }).reduceByKey((a, b) => a ++ b).map(outData =>
-      "2" + PunctuationConst.SPLITTER_STR + DataUtils.map2JSONString(outData._2)
-    ).saveAsTextFile(args(2))
-  }
-
-
-  def getFileData(prefixName: String): (String, Int, Array[String], Map[Int, String]) = {
-    val fileUtils: Properties = FileUtils.getPropertiesData()
-    val filePath: String = String.valueOf(fileUtils.get(prefixName + "_file_path"))
-    val keyColumnIndex = Integer.parseInt(String.valueOf(fileUtils.get(prefixName + "_key_index")))
-    val data4ColumnIndex = fileUtils.get(prefixName + "_column_index")
-    val data4ColumnIndexArray: Array[String] = StringUtils.split(String.valueOf(data4ColumnIndex), PunctuationConst.COMMA, -1)
-    val data4DataColumnIndex = String.valueOf(fileUtils.get(prefixName + "_data_column"))
-
-    val data4DataColumnMap: Map[Int, String] = Map()
-    for (combinationData <- StringUtils.split(data4DataColumnIndex, PunctuationConst.COMMA, -1)) {
-      val kvData = StringUtils.split(combinationData, PunctuationConst.VERTICAL_LINE, -1)
-      data4DataColumnMap.+(Integer.parseInt(kvData(0)) -> kvData(1))
+    def main(args: Array[String]): Unit = {
+        val configFileData1 = getFileData(args(0))
+        val conf = new SparkConf().setAppName("paSpark")
+        val sc = new SparkContext(conf)
+        val fileData1 = getRdd4File(configFileData1._1, sc, configFileData1._2, configFileData1._3, configFileData1._4)
+        val configFileData2 = getFileData(args(1))
+        val fileData2 = getRdd4File(configFileData2._1, sc, configFileData2._2, configFileData2._3, configFileData2._4)
+        fileData1.join(fileData2).map(value => {
+            val outKey = value._1
+            val outValue = value._2._1 ++ value._2._2
+            (outKey, outValue)
+        }).reduceByKey((a, b) => a ++ b).map(outData =>
+            "2" + PunctuationConst.SPLITTER_STR + DataUtils.map2JSONString(outData._2)
+        ).saveAsTextFile(args(2))
     }
 
-    (filePath, keyColumnIndex, data4ColumnIndexArray, data4DataColumnMap)
-  }
 
+    def getFileData(prefixName: String): (String, Int, Array[String], Map[Int, String]) = {
+        val fileUtils: Properties = FileUtils.getPropertiesData()
+        val filePath: String = String.valueOf(fileUtils.get(prefixName + "_file_path"))
+        val keyColumnIndex = Integer.parseInt(String.valueOf(fileUtils.get(prefixName + "_key_index")))
+        val data4ColumnIndex = fileUtils.get(prefixName + "_column_index")
+        val data4ColumnIndexArray: Array[String] = StringUtils.split(String.valueOf(data4ColumnIndex), PunctuationConst.COMMA, -1)
+        val data4DataColumnIndex = String.valueOf(fileUtils.get(prefixName + "_data_column"))
 
-  def getRdd4File(filePath: String, sc: SparkContext, keyIndex: Int, data4ColumnIndex: Array[String],
-                  data4DataColumnMap: Map[Int, String]): RDD[(String, Map[String, String])] = {
-    val dataFile = sc.textFile(filePath)
-    val value: RDD[(String, Map[String, String])] = dataFile.map(line =>
-      line.split(PunctuationConst.SPLITTER_USE)).map(dataArray => {
-      if (dataArray(0).equals("2")) {
-        scendDataDeal(data4DataColumnMap.get(keyIndex).toString, dataArray(1))
-      } else {
-        val outKey = dataArray(keyIndex)
-        val outMap: Map[String, String] = Map()
-
-        val outMapKeyList: util.ArrayList[String] = new util.ArrayList[String]()
-        for (columnIndex <- data4ColumnIndex) {
-          outMapKeyList.add(dataArray(Integer.parseInt(columnIndex)))
+        val data4DataColumnMap: Map[Int, String] = Map()
+        for (combinationData <- StringUtils.split(data4DataColumnIndex, PunctuationConst.COMMA, -1)) {
+            val kvData = StringUtils.split(combinationData, PunctuationConst.VERTICAL_LINE, -1)
+            data4DataColumnMap.+(Integer.parseInt(kvData(0)) -> kvData(1))
         }
 
-        val headColumnName = StringUtils.join(outMapKeyList, "__")
-        for (dataIndex <- data4DataColumnMap.keySet) {
-          outMap.+(headColumnName + "__" + data4DataColumnMap.get(dataIndex) -> dataArray(dataIndex))
-        }
+        (filePath, keyColumnIndex, data4ColumnIndexArray, data4DataColumnMap)
+    }
 
+
+    def getRdd4File(filePath: String, sc: SparkContext, keyIndex: Int, data4ColumnIndex: Array[String],
+                    data4DataColumnMap: Map[Int, String]): RDD[(String, Map[String, String])] = {
+        val dataFile = sc.textFile(filePath)
+        val value: RDD[(String, Map[String, String])] = dataFile.map(line =>
+            line.split(PunctuationConst.SPLITTER_USE)).map(dataArray => {
+            if (dataArray(0).equals("2")) {
+                scendDataDeal(data4DataColumnMap.get(keyIndex).toString, dataArray(1))
+            } else {
+                val outKey = dataArray(keyIndex)
+                val outMap: Map[String, String] = Map()
+
+                val outMapKeyList: util.ArrayList[String] = new util.ArrayList[String]()
+                for (columnIndex <- data4ColumnIndex) {
+                    outMapKeyList.add(dataArray(Integer.parseInt(columnIndex)))
+                }
+
+                val headColumnName = StringUtils.join(outMapKeyList, "__")
+                for (dataIndex <- data4DataColumnMap.keySet) {
+                    outMap.+(headColumnName + "__" + data4DataColumnMap.get(dataIndex) -> dataArray(dataIndex))
+                }
+
+                (outKey, outMap)
+            }
+        })
+
+        value
+    }
+
+    def scendDataDeal(keyName: String, data: String): (String, Map[String, String]) = {
+        val outMap: Map[String, String] = DataUtils.jsonString2Map(data)
+        var outKey: String = ""
+        if (outMap.contains(keyName)) {
+            outKey = outMap.get(keyName).toString
+        }
         (outKey, outMap)
-      }
-    })
-
-    value
-  }
-
-  def scendDataDeal(keyName: String, data: String): (String, Map[String, String]) = {
-    val outMap: Map[String, String] = DataUtils.jsonString2Map(data)
-    var outKey: String = ""
-    if (outMap.contains(keyName)) {
-      outKey = outMap.get(keyName).toString()
     }
-    (outKey, outMap)
-  }
 
-  def setHbaseData(rowKey: String, columnName: String, data: util.Map[String, String]): HBaseData = {
-    new HBaseData(rowKey, columnName, data)
-  }
+    def setHbaseData(rowKey: String, columnName: String, data: util.Map[String, String]): HBaseData = {
+        new HBaseData(rowKey, columnName, data)
+    }
 
-  def saveData2Hbase(tableName: String, hbaseData: HBaseData): Unit = {
-    val outPut: Put = HbaseUtils.data2Put(hbaseData)
-    val isSuccess = HbaseUtils.saveData2HBase(tableName: String, outPut)
-  }
+    def saveData2Hbase(tableName: String, hbaseData: HBaseData): Unit = {
+        val outPut: Put = HbaseUtils.data2Put(hbaseData)
+        val isSuccess = HbaseUtils.saveData2HBase(tableName: String, outPut)
+    }
 
 
 }
